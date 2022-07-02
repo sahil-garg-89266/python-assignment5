20a346364de7b4c6370c1a244a2a29004e2960ba
Sahil-Garg-bt19105103 <103331548+sahil-garg-89266@users.noreply.github.com>
2022-07-02 13:18:02 +0530

    Create 19105103_output.py

---
+++ 19105103_output.py
@@ -0,0 +1,141 @@
+Enter a string-:Hello there ! how are you?
+The original string  is :  Hello there ! how are you?
+The reversed string using a while loop is :  ?uoy era woh ! ereht olleH
+
+Enter your first no.-:4
+Enter your second no.-:150
+Enter the number which you will divide on entire range-:11
+All numbers in range (4, 150) which are divisible by 11 are as follows-:
+11
+22
+33
+44
+55
+66
+77
+88
+99
+110
+121
+132
+143
+
+Enter your first side:-1
+Enter your second side:-2
+Enter your third side:-3
+The sides entered don't for a triangle!!!
+Enter your first side:-4
+Enter your second side:-5
+Enter your third side:-6
+Area is equal to :- 9.921567416492215
+Enter your first side:-9
+Enter your second side:-2
+Enter your third side:-5
+The sides entered don't for a triangle!!!
+Enter your first side:-2
+Enter your second side:-7
+Enter your third side:-4
+Area is equal to :- (3.702531804508037e-16+6.046693311223912j)
+Enter your first side:-87
+Enter your second side:-12
+Enter your third side:-3
+The sides entered don't for a triangle!!!
+you were having only 5 turns to enter the sides of your triangle!!!
+
+The given pattern is as follows-:
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+Enter the number of rows: 14
+A
+BC
+DEF
+GHIJ
+KLMNO
+PQRSTU
+VWXYABC
+DEFGHIJK
+LMNOPQRST
+UVWXYABCDE
+FGHIJKLMNOP
+QRSTUVWXYABC
+DEFGHIJKLMNOP
+QRSTUVWXYABCDE
+
+please,Enter the range in which you want to get prime number
+Enter the Lowest Range Value-:0
+Enter the Upper Range Value-:100
+The Prime Numbers in range  (0, 100) are as follows-: 
+2
+3
+5
+7
+11
+13
+17
+19
+23
+29
+31
+37
+41
+43
+47
+53
+59
+61
+67
+71
+73
+79
+83
+89
+97
+
+Following are the numbers which are multiple of 7 and divisible by 11 in the range (1-500)-:
+77
+154
+231
+308
+385
+462
+
+you have to Enter 10 integers of your choice-:
+Enter an integer-:2
+Enter an integer-:6
+Enter an integer-:267
+Enter an integer-:84
+Enter an integer-:23
+Enter an integer-:09
+Enter an integer-:24
+Enter an integer-:54
+Enter an integer-:34
+Enter an integer-:65
+This is the list of integers entered by the user-: [2, 6, 267, 84, 23, 9, 24, 54, 34, 65]
+positive numbers are as follows-:2,6,267,84,23,9,24,54,34,65,
+negative numbers are as follows-:
+odd numbers are as follows-:267,23,9,65,
+even numbers are as follows-:2,6,84,24,54,34,
+Enter the number you want to count in list1-:24
+The number - 24 , occurs 1 times in the list1
+
+you can enter 10 words in list_enter
+Enter the word you want to add in the list_enter-:hello
+Enter the word you want to add in the list_enter-:there
+Enter the word you want to add in the list_enter-:hii
+Enter the word you want to add in the list_enter-:how
+Enter the word you want to add in the list_enter-:are
+Enter the word you want to add in the list_enter-:you
+Enter the word you want to add in the list_enter-:doing
+Enter the word you want to add in the list_enter-:hoping
+Enter the word you want to add in the list_enter-:you
+Enter the word you want to add in the list_enter-:are
+Enter the word you want to count in list_enter-:doing
+word - doing , occurs 1 times
